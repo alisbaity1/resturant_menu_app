62a69bf205cd810eb0edf622bc0e9c626bc42166
alisbaity1 <alisbaity44@gmail.com>
2024-07-22 11:11:41 +0300

    nothing

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -3,7 +3,7 @@ import 'cart_page.dart';  // Import the CartPage class
 
 class HomePage extends StatefulWidget {
   @override
-  _HomePageState createState() => _HomePageState();
+  _HomePageState createState() => _HomePageState(); 
 }
 
 class _HomePageState extends State<HomePage> {
